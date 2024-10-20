79136fed33afb8bf43bbc0b840e93d4186dfa53f
Wagner de Paula Sobrinho <133580990+WagnerSobrinho@users.noreply.github.com>
2024-10-20 00:23:23 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,17 +24,145 @@ st.write( '# La Place Company Growth Dashboard' )
 
 st.markdown(
     """
-        Growth Dashboad foi construido para acompanhar as métricas de crescimento de Entregadores e Restaurantes.
-        ### Como utilizar este Dashboard.
-        - Visão Empresa
-            - Visão Gerencial:   Métricas gerais de comportamento.
-            - Visão Tática:      Indicadores semanais de crescimento.
-            - Visão Geográfica:  Insights de geolocalização.
-        - Visão Entregadores
-            - Acompanhamento dos indicadores semanais de crescimento.
-        - Visão Restaurantes
-            - Acompanhamento dos indicadores semanais de crescimento dos restaurantes.
-        ### Ask for help
-        - Time de Data Science no Discord
-            - wagnersobrinho    
+        # La_Place_Company
+
+# Problema de Negócio 
+
+ 
+
+A La Place Company é uma empresa de tecnologia que criou um aplicativo que conecta restaurantes, entregadores e pessoas. 
+
+Através desse aplicativo, é possível realizar o pedido de uma refeição, em qualquer restaurante cadastrado, e recebê-lo no conforto da sua casa por um entregador também cadastrado no aplicativo da La Place Company.  
+
+A empresa possui um modelo de negócio chamado Marketplace, que faz o intermédio do negócio entre três clientes principais: Restaurantes, entregadores e pessoas compradoras e gera muitos dados sobre entregas, tipos de pedidos, condições climáticas, avaliação dos entregadores e etc.  
+
+
+     
+
+## O Desafio 
+
+Você foi contratado como um Cientista de Dados para criar soluções de dados para entrega, mas antes de treinar algoritmos, a necessidade da empresa é ter os principais KPIs estratégicos organizados em uma única ferramenta, para que o CEO possa consultar e conseguir tomar decisões simples, porém importantes. 
+
+ Para acompanhar o crescimento desses negócios, o CEO gostaria de ver as seguintes métricas de crescimento: 
+
+  
+
+## 1. Visão Empresa 
+ Visão Gerencial: Métricas gerais de comportamento. 
+ Visão Tática: Indicadores semanais de crescimento. 
+ Visão Geográfica: Insights de geolocalização 
+
+   
+## 2. Visão Entregadores 
+ Acompanhamento dos indicadores semanais de crescimento   
+
+  
+## 3. Visão Restaurantes 
+ Acompanhamento dos indicadores semanais de crescimento dos restaurantes. 
+
+    
+
+O CEO também pediu que fosse gerado dashboards que permitisse que ele visualizasse as principais métricas solicitadas. O CEO precisa dessas informações o mais rápido possível, uma vez que ele também é novo na empresa e irá utilizá-las para entender melhor a empresa e conseguir tomar decisões mais assertivas. 
+
+  
+O meu primeiro objetivo como Cientista de Dados foi criar um conjunto de gráficos e/ou tabelas que exibam essas métricas e criar os dashboards solicitados pelo CEO, utilizando os dados que a La Place possuía. 
+
+
+	 
+ 
+
+## Premissas do Dashboard: 
+
+1. Os dados utilizados para criação deste Dashboard foram obtidos via plataforma Kaggle: 
+https://www.kaggle.com/datasets/gauravmalik26/food-delivery-dataset? resource=download& select=train.csv 
+
+ 
+2. O Dashboard foi construído para acompanhar as métricas de crescimento de Entregadores e Restaurantes, além de ajudar em insights para alavancar o crescimento do negócio. Disponibilizado alguns KPI's iniciais ao dashboard, podendo ser agregado outros; 
+
+ 
+3. Os indicadores foram agrupados por algumas perspectivas de negócio: 
+
+ 
+
+	- Visão Empresa: 
+        - Visão Gerencial: Métricas gerais de comportamento com os Pedidos por dia, Pedidos por densidade de trânsito, Pedidos por tipo de cidade.  
+        - Visão Tática: Pedidos por semana, Pedidos por tipo de entrega.
+        - Visão Geográfica:  Insights de geolocalização dos consumidores. 
+
+  
+	- Visão Entregadores:
+        - Principais métricas, Métricas Gerais, Avaliação Média por entregador, Avaliação Média por tipo trânsito, Avaliação Média por clima, Velocidade de Velocidade: Top entregadores mais rápido, Top Entregadores mais lento. 
+
+            
+	- Visão Restaurantes:
+        - Principais métricas, Métricas Gerais: Total de Entregadores cadastrados, Distância Média percorrida (KM), Tempo Médio de entrega com festival, Desvio Médio Padrão de entrega com festival, Tempo Médio de entrega sem festival, Desvio Médio Padrão de entrega sem festival. Tempo Médio de entrega por cidades, Tempo Médio e Desvio Padrão de entrega por cidade, Distância média e Desvio Padrão de entrega por cidade, Distância Média e o Desvio Padrão de entrega por cidade e tipo de pedido.     
+
+ 
+
+       
+
+## Como utilizar este Dashboard. 
+
+- Na barra lateral é possível realizar alguns filtros, como:
+    - escolher um período de data limite;
+    - filtrar por condição do trânsito. 
+
+       
+
+ 
+
+## Alguns Insights de dados. 
+
+- 	Quantidade de pedidos por dia.
+- 	Quantidade de pedidos por semana.
+- 	Distribuição dos pedidos por tipo de tráfego. 
+- 	Comparação do volume de pedidos por cidade e tipo de tráfego. 
+- 	A quantidade de pedidos por entregador por semana. 
+- 	A localização central de cada cidade por tipo de tráfego. 
+- 	A menor e maior idade dos entregadores. 
+- 	A pior e a melhor condição de veículos.
+- 	A avaliação média por entregador. 
+- 	A avaliação média e o desvio padrão por tipo de tráfego. 
+- 	A avaliação média e o desvio padrão por condições climáticas. 
+- 	Os 10 entregadores mais rápidos por cidade. 
+- 	Os 10 entregadores mais lentos por cidade. 
+- 	A quantidade de entregadores únicos. 
+- 	A distância média dos restaurantes e dos locais de entrega. 
+- 	O tempo médio e o desvio padrão de entrega por cidade. 
+- 	O tempo médio e o desvio padrão de entrega por cidade e tipo de pedido.
+- 	O tempo médio e o desvio padrão de entrega por cidade e tipo de tráfego. 
+- 	O tempo médio de entrega durantes os Festivais. 
+
+ 
+
+  
+
+## O produto final do Projeto. 
+
+Painel online, hospedado em Cloud e disponível para acesso em qualquer dispositivo conectado à internet. 
+
+O painel pode ser acessado através desse link: https://wagnersobrinho-project-la-place-company.streamlit.app/ 
+
+  
+
+## Conclusão. 
+
+O objetivo deste projeto é criar um conjunto de gráficos e / ou tabelas que exibam essas métricas da melhor forma possível para o CEO entender melhor o negócio para conseguir tomar as melhores decisões estratégicas e alavancar ainda mais a empresa. 
+
+  
+
+## Próximos passos:  
+
+- Realizar uma nova avaliação global dos dados para obter mais insights para o negócio;
+- Aumentar o número de gráficos com outras perspectivas de análises. 
+
+             
+
+ 
+
+Ask for help 
+
+- Time de Data Science no Discord 
+
+  - wagnersobrinho       
     """)
